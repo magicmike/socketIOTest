1e8941ed25dbb84e825ac5b3236c3b4946001157
Dirk <dirk.swart@wickeddevice.com>
2015-01-20 12:32:03 -0500

    change 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-   res.send('<h3>Hello World</h3>');
+   res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, function () {
